bad7842e712ecaa6056ef44aeeac953263d4ef12
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-12-20 16:41:07 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
+//@ts-check 
 import React from "react";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<Navbar/>
+			<div className="container">
+				<div className="row">
+					<Jumbotron/>
+				</div>
+				<div className="row">
+					
+					<div className="col-3"> 
+						<Card
+							img="https://picsum.photos/400/300?random"
+						/> 
+					</div>
+					<div className="col-3"> 
+						<Card
+							img="https://picsum.photos/400/300?random"
+						/> 
+					</div>
+					<div className="col-3"> 
+						<Card
+							img="https://picsum.photos/400/300?random"
+						/> 
+					</div>
+					<div className="col-3"> 
+						<Card
+							img="https://picsum.photos/400/300?random"
+						/> 
+					</div>
+
+				</div>
+			</div>
+
 		</div>
 	);
 };
